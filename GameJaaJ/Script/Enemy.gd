81b221761ebe72b74c2bddff6432639c47e1808c
kinge880 <brunomaya10@hotmail.com>
2019-07-14 12:19:14 -0500

    atualização
    adicionado atk ao player + script basico de perseguição do enemy

---
+++ GameJaaJ/Script/Enemy.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+export var speed = 200
+onready var raycast = $RayCast2D
+var player = null
+
+func _ready():
+	add_to_group("enemy")
+	
+func _physics_process(delta):
+	if player == null:
+		return
+		
+	var to_player = player.global_position - global_position
+	print("oi")
+	to_player = to_player.normalized()
+	global_rotation = atan2(to_player.y, to_player.x)
+	move_and_collide(to_player * speed * delta)
+	
+	if raycast.is_colliding():
+		var collision = raycast.get_collider()
+		print(collision)
+		if collision.name == "Player":
+			collision._death()
+
+func _kill():
+	queue_free()
+
+func _set_player(body):
+	player = body
---
+++ GameJaaJ/Script/Player.gd
@@ -0,0 +1,68 @@
+extends KinematicBody2D
+
+export var speed = 300
+var velocity = Vector2()
+onready var raycast = $RayCast2D
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	get_tree().call_group("enemy", "_set_player", self)
+	
+func _physics_process(delta):
+	velocity = Vector2()
+	look_at(get_global_mouse_position())
+	var look_mouse = get_global_mouse_position() - global_position
+	
+	#Movimentos basicos
+	#movimentação de teste 1
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2(0, speed)
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(0, -speed)
+	if Input.is_action_pressed("ui_left"):
+		velocity = Vector2(-speed, 0)
+	if Input.is_action_pressed("ui_right"):
+		velocity = Vector2(speed, 0)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity = Vector2(0, 0)
+	
+	"""
+	 #movimentação de teste 2
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity = Vector2(0, 0)
+	"""
+	"""
+	#movimentação de teste 3
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += speed
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
+		"""
+	velocity = move_and_collide(velocity * delta)
+	
+	#atk
+	if Input.is_action_pressed("atk"):
+		var collision = raycast.get_collider()
+		print(collision)
+		if raycast.is_colliding() and collision.has_method("_kill"):
+			collision._kill()
+
+func _death():
+	get_tree().reload_current_scene()
