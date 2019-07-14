2917177cca8df4737c5e3e2a3f5d9cb83c8bbc24
kinge880 <brunomaya10@hotmail.com>
2019-07-14 13:32:28 -0500

    Comentarios
    Só pra organizar mais

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,31 +1,33 @@
 extends KinematicBody2D
 
 export var speed = 300
-var motion = Vector2()
+var velocity = Vector2()
 onready var raycast = $RayCast2D
 
 func _ready():
 	yield(get_tree(), "idle_frame")
+	#passa a instancia de player a todos no grupo "enemy" que possuem a função "_set_player"
 	get_tree().call_group("enemy", "_set_player", self)
 	
-func _process(delta):
-	motion = Vector2()
+func _physics_process(delta):
+	velocity = Vector2()
 	look_at(get_global_mouse_position())
 	var look_mouse = get_global_mouse_position() - global_position
 	
-	#Movimentos basicos
+	#Movimentos basicos, fiz 3 deles pq to na duvida de qual fica melhor, escolhemos dps
 	#movimentação de teste 1
 	if Input.is_action_pressed("ui_down"):
-		motion += Vector2(0, 1)
+		velocity = Vector2(0, speed)
 	if Input.is_action_pressed("ui_up"):
-		motion += Vector2(0, -1)
+		velocity = Vector2(0, -speed)
 	if Input.is_action_pressed("ui_left"):
-		motion += Vector2(-1, 0)
+		velocity = Vector2(-speed, 0)
 	if Input.is_action_pressed("ui_right"):
-		motion += Vector2(1, 0)
-	
-	motion = motion.normalized() * speed
-	
+		velocity = Vector2(speed, 0)
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		velocity = Vector2(0, 0)
 	
 	"""
 	 #movimentação de teste 2
@@ -54,15 +56,15 @@ func _process(delta):
 		velocity.x = 0
 		velocity.y = 0
 		"""
-	#move_and_collide(motion * delta)
-	move_and_slide(motion)
+	velocity = move_and_collide(velocity * delta)
 	
-	#atk
+	#atacar, podemos modificar aqui pra fazer do jeito que preferimos, deixei assim de inicio pra ter uma base
 	if Input.is_action_pressed("atk"):
 		var collision = raycast.get_collider()
 		print(collision)
 		if raycast.is_colliding() and collision.has_method("_kill"):
 			collision._kill()
 
+#função para ativar a situação escolhida de "morte"
 func _death():
 	get_tree().reload_current_scene()
