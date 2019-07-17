d139075eaac89475efcbe80b7d398e62a00f830e
kinge880 <brunomaya10@hotmail.com>
2019-07-16 23:36:05 -0500

    att
    att

--- Script/MainMenu.gd
+++ Script/MainMenu.gd
@@ -1,11 +1,14 @@
 extends Node2D
+var count = 0
 
 func _ready():
 	$AnimationPlayer.play("entry")
-
+	
 func _on_NewGame_pressed():
 	get_tree().change_scene("res://Word.tscn")
 
-
 func _on_Exit_pressed():
 	get_tree().quit()
+	
+
+
