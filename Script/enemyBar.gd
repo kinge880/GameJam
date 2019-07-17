134387d6e0b18aa3d23a29e3d8d656ba3ef89b8b
kinge880 <brunomaya10@hotmail.com>
2019-07-16 20:26:42 -0500

    merge
    merge

--- Script/enemyBar.gd
+++ Script/enemyBar.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 func _ready():
-	pass
+	$EnemyLifeBar.hide()
 
 func _update_lifeBar(life):
 	print(life)
-	$EnemyLifeBar.value = life
-	#$LifeBarTween.interpolate_property($EnemyLifeBar,'value', $EnemyLifeBar.value, life, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	#$LifeBarTween.start()
+	#$EnemyLifeBar.value = life
+	$LifeBarTween.interpolate_property($EnemyLifeBar,'value', $EnemyLifeBar.value, life, 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$LifeBarTween.start()
 	#$AnimationPlayer.play("lifeBar_flash")
 	
+	if (life < 100):
+		$EnemyLifeBar.show()
 	"""
 	if life < 40:
 		$Margin/Recipe/LifeBar.tint_progress = Color(1, 0, 0)
