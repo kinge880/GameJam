031ca7b651a96da827e0bc3b4d42706cd771f211
kinge880 <brunomaya10@hotmail.com>
2019-07-17 21:32:22 -0500

    att
    att

--- Script/Map.gd
+++ Script/Map.gd
@@ -1,17 +1,16 @@
 extends Node
-"""
+
 func _ready():
     set_camera_limits()
 	
 #define um limite maximo para a camera
 func set_camera_limits():
-    var map_limits = $Ground.get_used_rect()
-    var map_cellsize = $Ground.cell_size
+    var map_limits = $GroundBruno.get_used_rect()
+    var map_cellsize = $GroundBruno.cell_size
     $Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
     $Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
     $Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
     $Player/Camera2D.limit_bottom = map_limits.end.y * map_cellsize.y
-"""
 
 #o map em si vai controlar a gerenciar a bala, pq se deixarmos isso sobre o player, bala vai receber o 
 #transform do player e se mover no ar quando o player se mover
