578794037379860fdd7f2e8336c55eb74b300cec
kinge880 <brunomaya10@hotmail.com>
2019-07-19 21:15:13 -0500

    att
    att

--- Script/teste/LoadStarGame.gd
+++ Script/teste/LoadStarGame.gd
@@ -4,4 +4,4 @@ func _ready():
 	$AnimationPlayer.play("entry")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	get_tree().change_scene("res://ui/MainMenu.tscn")
+	get_tree().change_scene("res://ui/Menu.tscn")
